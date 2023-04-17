de82b9b16fd35dd2c1ca1dfd80ea7d94bb8e844d
장 형수 <onlyhisson@hackersholdings.com>
2023-04-17 18:14:31 +0900

    [dev] global css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css"; // 글로벌 스타일 적용은 _app.tsx 에서만 import
+import "@/styles/styles.css";
+import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
